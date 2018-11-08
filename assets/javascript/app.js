06d2f2c2d60e24a31bed3a6d3b56de57a2ea21f1
Heidijvr <43320863+Heidijvr@users.noreply.github.com>
2018-11-08 11:24:30 -0800

    Working Gif's

---
+++ assets/javascript/app.js
@@ -0,0 +1,109 @@
+
+//Gifs play and pause!!!
+var gifClick = function() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+};
+
+//Event listener for all button elements
+// Gif display triggered by button!!!
+var buttonClick = function()  {
+    
+    //this refers to button that was clicked
+    var person = $(this).attr("data-person");
+    console.log('Clicked:', person);
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        encodeURI(person) + "&api_key=NyITjycRvRjs0DSBiSLflQ0nO8ONHB7X&limit=10";
+
+    console.log(queryURL);
+
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    }).then(function(response) {
+        $("#gifs-appear-here").empty();
+        var results = response.data;
+        
+        for (var i = 0; i < results.length; i++) {
+            console.log(results[i].rating);
+
+            var imageResult = results[i].images;
+            console.log(imageResult.fixed_width.url);
+            var imageToAdd = $("<img>");
+            imageToAdd.addClass("gif");
+            imageToAdd.attr("src", imageResult.fixed_width_still.url);
+           
+            imageToAdd.attr("data-animate", imageResult.fixed_width.url);
+            imageToAdd.attr("data-still", imageResult.fixed_width_still.url);
+            imageToAdd.attr("data-state", "still");
+         
+
+            imageToAdd.on('click', gifClick);
+            $("#gifs-appear-here").append(imageToAdd);
+        } 
+
+    });
+};
+
+
+var topics = ["John Cleese", "Ricky Gervais", "Sacha Baron Cohen", "Jerry Seinfeld"];
+
+var renderButtons = function() {
+    $("#buttons").empty();
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+        var buttonToAdd = $("<button>");
+        buttonToAdd.text(topics[i]);
+        buttonToAdd.attr("data-person", topics[i]);
+        buttonToAdd.on('click', buttonClick);
+
+        $("#buttons").append(buttonToAdd);
+    }
+}
+
+renderButtons();
+
+$("#submit").on("click", function() {
+    var name = $("#nameInput").val().trim();
+    topics.push(name);
+    renderButtons();
+}); 
+
+/*
+    //Ajax GeEt request
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    })
+    // After data comes back from the API
+    .then(function(response) {
+        // Storing an array of results in the results variable
+        var results = response.data;
+        // looping over every result item
+        for (var i = 0; i <results.length; i++) {
+
+            //add rating!!!
+            // Creating image tag
+            var personImage = $("<img>");
+
+            // GIving image tag an src attribute of a property pulled from the reult item
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            // Appending the paragraph and personImage to the gifDiv we created
+            gifDIv.append(p);
+            gifDiv.append(personImage);
+
+            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+            $("#gifs-appear-here").prepend(gifDiv);
+
+        }
+
+*/
